aff092c374722a0c02c90afaa26d845ef236e09d
Cynthia Tristán <cyntrist@ucm.es>
2024-10-04 11:37:20 +0200

    11 y 12 problemas tema 1 entregable ej tipo examen

--- container/p2/ejercicio2/write_strings.c
+++ container/p2/ejercicio2/write_strings.c
@@ -3,8 +3,44 @@
 #include <string.h>
 #include <err.h>
 
-int main(int argc, char* argv[])
+// fopen(),fclose(), fread(), fwrite(), fseek() y malloc()
+
+int main(int argc, char *argv[])
 {
-	/* To be completed */
+	FILE *file;
+
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s <file_name> <content> <content>\n", argv[0]);
+		exit(1);
+	}
+
+	if ((file = fopen(argv[1], "w")) == NULL)
+		err(2, "The input file %s could not be opened", argv[1]);
+
+
+
+
+
+
+
+	while ((n = fread(&c, sizeof(c), 1, file)) == 1)
+	{
+		fwrite(&c, sizeof(c), 1, stdout);
+
+		if (n == 0)
+		{
+			fclose(file);
+			err(3, "fread() failed!!");
+		}
+	}
+	fwrite(&"\0", 1, 1, stdout);
+
+
+
+
+
+
+	fclose(file);
 	return 0;
 }
