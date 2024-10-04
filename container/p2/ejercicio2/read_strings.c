59e608d2d5cfb6e0f7f39879698b1afff8c27bd1
Cynthia Tristán <cyntrist@ucm.es>
2024-10-04 14:47:52 +0200

    NO SE!

--- container/p2/ejercicio2/read_strings.c
+++ container/p2/ejercicio2/read_strings.c
@@ -25,13 +25,14 @@ char *loadstr(FILE *file)
 	fseek(file, ini, SEEK_SET); // vuelve al principio del archivo y limpia el EOF si procede
 
 	char *string;
-	if (!(string = (char *)malloc(length + 1)))
+	if (!(string = (char*) malloc(length + 1)))
 	{ // https://www.ibm.com/docs/en/xl-c-and-cpp-aix/16.1?topic=promotions-boolean-conversions
 		perror("Memory allocation error");
 		exit(EXIT_FAILURE);
 	}
 
-	if (fread(string, 1, length + 1, file) != length + 1) // leer el string una vez
+	size_t read = fread(string, 1, length + 1, file);
+	if (read != length + 1)
 		return NULL;
 
 	return string; // devuelve un puntero al string leido
@@ -50,12 +51,37 @@ int main(int argc, char *argv[])
 	if ((file = fopen(argv[1], "rb")) == NULL)
 		err(2, "The input file %s could not be opened", argv[1]);
 
-	char *line;
-	while ((line = loadstr(file)) != NULL)
+
+
+	// he estado leyendo que hacer while(!feof()) es mala practica y 
+	// encima no estoy haciendo uso del retorno NULL de la funcion loadstr()
+	// while (!feof(file))
+	// {
+	// 	char *line = loadstr(file);
+	// 	printf("%s\n", line);
+	// 	free(line);
+	// 	if (feof(file))
+	// 		break;
+	// }
+
+	// Esto no me funciona, no imprime infinitamente pero el programa nunca acaba 
+	// entiendo que es porque libero line cada vez
+	// while (line != NULL)
+	// {
+	// 	line = loadstr(file);
+	// 	printf("%s\n", line);
+	// 	free(line);
+	// }
+
+	while (!feof(file))
 	{
+		char *line = loadstr(file);
 		printf("%s\n", line);
 		free(line);
+		if (feof(file))
+			break;
 	}
+
 
 	fclose(file);
 	printf("Read of file: %s\n", argv[1]);
