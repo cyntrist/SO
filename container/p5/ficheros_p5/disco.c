cca235ae000ee60a07092844da295fd43e255021
Cynthia Tristán <cyntrist@ucm.es>
2024-12-08 19:01:52 +0100

    auuuuugh

--- container/p5/ficheros_p5/disco.c
+++ container/p5/ficheros_p5/disco.c
@@ -2,48 +2,84 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <time.h>
 
-#define CAPACITY 5
+#define CAPACITY 3
 #define VIPSTR(vip) ((vip) ? "  vip  " : "not vip")
 
+/// STRUCTS
+/// @brief parámetros de las opciones del programa
 struct options
 {
 	FILE *input_file;
 };
 
-pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
+/// @brief estructura para los datos de un cliente de la discoteca
+struct client
+{
+	int id;
+	int vip;
+	pthread_t thread;
+};
+
+/// SHARED DATA
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t full = PTHREAD_COND_INITIALIZER;
+pthread_cond_t vip_queue = PTHREAD_COND_INITIALIZER;
+pthread_cond_t normal_queue = PTHREAD_COND_INITIALIZER;
+int num_clients_inside = 0;
+int num_vip_waiting = 0;
+int num_normal_waiting = 0;
 
 void enter_normal_client(int id)
 {
+	pthread_mutex_lock(&mutex);
+	num_clients_inside++;
+	num_normal_waiting--;
+	pthread_mutex_unlock(&mutex);
 }
 
 void enter_vip_client(int id)
 {
+	pthread_mutex_lock(&mutex);
+	num_clients_inside++;
+	num_vip_waiting--;
+	pthread_mutex_unlock(&mutex);
 }
 
 void dance(int id, int isvip)
 {
-	printf("Client %2d (%s) dancing in disco\n", id, VIPSTR(isvip));
+	printf("Client %2d (%s) dancing at the disco\n", id, VIPSTR(isvip));
 	sleep((rand() % 3) + 1);
 }
 
-void disco_exit(int id, int isvip)
+void exit_client(int id, int isvip)
 {
+	pthread_mutex_lock(&mutex);
+
+	printf("Client %d has exited the disco.\n", id);
+	num_clients_inside--;
+
+	if (num_vip_waiting > 0)
+		pthread_cond_broadcast(&vip_queue);
+	else if (num_normal_waiting > 0)
+		pthread_cond_broadcast(&normal_queue);
+
+	pthread_mutex_unlock(&mutex);
 }
 
 void *client(void *arg)
 {
-	// ...
+	struct client *this = (struct client *)arg;
+	int id = this->id;
+	int isvip = this->vip;
 
-	// 	if (isvip)
-	// 		enter_vip_client(...);
-	// else enter_normal_client(...);
-	// dance(...);
-	// exit_client(...);
-
-	// ...
+	if (isvip)
+		enter_vip_client(id);
+	else
+		enter_normal_client(id);
+	dance(id, isvip);
+	exit_client(id, isvip);
 }
 
 int main(int argc, char *argv[])
@@ -72,19 +108,61 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	// parseo
-	int buff, m;
-	fscanf(option.input_file, "%d", &m);
-	int clients[m], i = 0;
-	while (fscanf(option.input_file, "%d", &clients[i]) == 1) 
+	/// parseo
+	// int buff, m;
+	// fscanf(option.input_file, "%d", &m);
+	// int vip_list[m], i = 0;
+	// while (fscanf(option.input_file, "%d", &vip_list[i]) == 1)
+	// {
+	// 	printf("Client number %d is %s\n", i, VIPSTR(vip_list[i]));
+	// 	i++;
+	// }
+
+	int buff, M;
+	fscanf(option.input_file, "%d", &M);
+	struct client clients[M];
+	for (int i = 0; i < M; i++)
 	{
-		printf("Client number %d is %s\n", i, VIPSTR(clients[i]));
-		i++;
-	}
+		clients[i].id = i;
+		fscanf(option.input_file, "%d", &clients[i].vip);
+		printf("Client number %d is %s\n", i, VIPSTR(clients[i].vip));
+	};
 	// ---
 
+	srand(time(NULL)); // Seed random number generator for usleep
 
+	for (int i = 0; i < M; i++)
+	{
+		if (pthread_create(&clients[i].thread, NULL, client, (void *)&clients[i]) == 0)
+			;
+		// printf("Thread %d created successfully.\n", i);
+	}
 
+	for (int i = 0; i < M; i++)
+	{
+		if (pthread_join(clients[i].thread, NULL) == 0)
+			;
+		// printf("Thread %d joined successfully.\n", i);
+	}
+
+	// // Initialize mutexes for chopsticks
+	// for (i = 0; i < N; i++)
+	// {
+	// 	pthread_mutex_init(&mutex, NULL);
+	// }
+
+	// // Destroy mutexes (though unreachable in this infinite loop)
+	// for (i = 0; i < N; i++)
+	// {
+	// 	pthread_mutex_destroy(&mutex);
+	// }
+
+	// for (int i = 0; i < M; i++)
+	// {
+	// 	free(clients[i]);
+	// }
+
+	fclose(option.input_file);
 
 	return 0;
 }
